cf9a56ac447dfc455cbd0a991f0876da0bd82e4e
Ashen Madhusanka <madhu@Ashens-MacBook-Air-2.local>
2023-12-21 17:09:51 +0530

    Initial commit

---
+++ src/main/java/Task/Manager/Spring/Boot/mexxar/Task/Manager/Spring/Boot/TaskManagerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package Task.Manager.Spring.Boot.mexxar.Task.Manager.Spring.Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Task/Manager/Spring/Boot/mexxar/Task/Manager/Spring/Boot/TaskManagerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package Task.Manager.Spring.Boot.mexxar.Task.Manager.Spring.Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
